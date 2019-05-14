e9ccf9a6ea6d5a56f6558cdfcd936498e471550e
26cs099 <50616120+26cs099@users.noreply.github.com>
2019-05-14 16:25:25 +0530

    Add files via upload

---
+++ vow.py
@@ -0,0 +1,9 @@
+# your code goes here# your code goes here
+x=str(input())
+if(x>='a' and x<='z'):
+	if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
+		print("vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
