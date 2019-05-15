1dfbd92f3889654ae65177fdf6e1a48f961e3c63
26cs099 <50616120+26cs099@users.noreply.github.com>
2019-05-15 13:32:14 +0530

    Add files via upload

---
+++ great.py
@@ -0,0 +1,13 @@
+# your code goes here
+try:
+	a=int(input())
+	b=int(input())
+	c=int(input())
+	if(a>b and a>c):
+		print(a)
+	elif(b>c):
+		print(b)
+	else:
+		print(c)
+except ValueError:
+	print("invalid")
