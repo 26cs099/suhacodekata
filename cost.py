58e0633b7b5e6a98ab401017abe34ee38d3c1bb2
26cs099 <50616120+26cs099@users.noreply.github.com>
2019-07-19 19:08:51 +0530

    Update cost.py

--- cost.py
+++ cost.py
@@ -1,9 +1,9 @@
 # your code goes here
 s,m=map(str,input().split())
-cost=abs(len(s)-len(m))
+costset=abs(len(s)-len(m))
 for k in range(len(s)):
 	if(len(s)==1 and s[j] in m):
 		break
 	if(s[j] != m[j]):
-		cost+=1
-print(cost)
+		costset+=1
+print(costset)
